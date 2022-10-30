1559b6822bfbb71426ca09c2be189fcf043e0801
mustafatuc <mustafatuc1@gmail.com>
2022-10-30 19:59:22 +0300

    Add project files.

---
+++ DataAccesLayer/Concrete/Repositories/CategoryRepository.cs
@@ -0,0 +1,40 @@
+﻿using DataAccesLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccesLayer.Concrete.Repositories
+{
+    public class CategoryRepository : ICategoryDal
+    {
+        Context c = new Context();
+       DbSet<Category> _object;
+        public void Delete(Category p)
+        {
+            _object.Remove(p);
+            c.SaveChanges();
+        }
+
+        public void Insert(Category p)
+        {
+            _object.Add(p);
+            c.SaveChanges();
+        }
+
+        public List<Category> List()
+        {
+          return _object.ToList();
+            
+        }
+
+        public void Update(Category p)
+        {
+            c.SaveChanges();
+        }
+    }
+}
---
+++ DataAccesLayer/Concrete/Repositories/WriterRepository.cs
@@ -0,0 +1,17 @@
+﻿using DataAccesLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccesLayer.Concrete.Repositories
+{
+    public class WriterRepository : IWriterDal
+    {
+      
+    }
+}
